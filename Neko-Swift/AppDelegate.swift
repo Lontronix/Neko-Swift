22ece5c6d7f714716b8f4a543a78af6d693ffa25
Lonnie Gerol <lonnie@lonniegerol.com>
2023-04-08 21:52:39 -0400

    Option Clicking on menu item now auto hides Neko

--- Neko-Swift/AppDelegate.swift
+++ Neko-Swift/AppDelegate.swift
@@ -43,19 +43,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             toggleNekoMenuItem.title = currentWindowState.title
             statusItem?.button?.image = currentWindowState.icon
             configureStatusItem()
-            
-            if currentWindowState == .hidden {
-                print("hiding window...")
-            }
         }
+    }
+    
+    var optionKeyPressed: Bool {
+        return NSEvent.modifierFlags.contains(.option)
     }
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         toggleNekoMenuItem.title = currentWindowState.title
         configureStatusItem()
     }
-
+    
     @IBAction func toggleNekoMenuItemClicked(_ sender: NSMenuItem) {
+        toggleNeko()
+    }
+    
+    private func toggleNeko() {
         switch currentWindowState {
         case .visible:
             currentWindowState = .hidden
@@ -71,21 +75,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         )
     }
     
+    @objc private func statusBarButtonClicked(sender: NSStatusBarButton) {
+        if optionKeyPressed {
+            toggleNeko()
+        } else {
+            sender.menu?.popUp(positioning: nil, at: CGPoint(x: -1, y: sender.bounds.maxY + 5), in: sender)
+        }
+    }
+    
     private func configureStatusItem() {
-        let bundle = Bundle.main
         let image = currentWindowState.icon
         image.size = NSSize(width: 22, height: 22)
-        
         let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
         guard let statusItemButton = statusItem.button else { return }
         statusItemButton.image = image
         
-        statusItem.menu = statusMenu
-        
+        statusItem.button?.menu = statusMenu
+        statusItem.button?.action = #selector(statusBarButtonClicked(sender:))
         
         self.statusItem = statusItem
     }
-
-
 }
 
